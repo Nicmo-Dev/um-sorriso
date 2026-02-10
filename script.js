f9974766a861441ad356273baa71cabd368cd583
Nicmo-Dev <nicmocontatoprofissional@gmail.com>
2026-02-09 23:03:07 -0300

    feat: audio em loop na pagina do meme

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function goToMeme() {
+  localStorage.setItem("playSax", "true");
   window.location.href = "meme.html";
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const audio = document.getElementById("sax");
+
+  if (audio && localStorage.getItem("playSax") === "true") {
+    audio.volume = 0.35;
+    audio.play();
+  }
+});
